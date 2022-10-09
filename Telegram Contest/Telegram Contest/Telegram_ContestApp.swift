21f97336b407d648a62143e5a665ff53ae37f345
Vladislav Shilov <vladislav@luno.com>
2022-10-09 20:18:21 +0200

    Create a project

---
+++ Telegram Contest/Telegram Contest/Telegram_ContestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Telegram_ContestApp.swift
+//  Telegram Contest
+//
+//  Created by Vladik Shilov on 9.10.22.
+//
+
+import SwiftUI
+
+@main
+struct Telegram_ContestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
